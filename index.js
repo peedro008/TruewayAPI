49183f1cc3dc9f8f1ac83192d7a540173859d5bd
peedro008 <pedro39661@gmail.com>
2022-05-03 19:18:41 -0300

    sad

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { sequelize } = require('./src/db')
 // // const{createClient}=require("./src/data/clients")
 
 // Syncing all the models at once.
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   server.listen(8080, async()=>{
    
     try{
