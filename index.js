4772bd948b0f2b62bc9409dce0afccf65cc96629
peedro008 <pedro39661@gmail.com>
2022-05-16 13:45:10 -0300

    asdas!

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const { sequelize } = require('./src/db')
 
 
 // Syncing all the models at once.
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   server.listen(8080, async()=>{
    
     try{
      
-        Promise.all([Pe()])
-        .then(res =>console.log("datos cargados!"))      
+      //   Promise.all([Pe()])
+      //   .then(res =>console.log("datos cargados!"))      
       
-       console.log('%s listening at 4000'); 
+      //  console.log('%s listening at 4000'); 
       
     }
     catch(e){
