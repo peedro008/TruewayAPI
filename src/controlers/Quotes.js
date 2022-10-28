cb746f81805e32d9a7eb37f744dbaf246826c291
peedro008 <pedro39661@gmail.com>
2022-10-27 21:14:54 -0300

    asd1112

--- src/controlers/Quotes.js
+++ src/controlers/Quotes.js
@@ -597,7 +597,6 @@ const getUsersAverage = async (req, res) => {
     let dateTo = req.query.dateTo;
     let result 
     let quotes
-    let err = 0
     if(!dateFrom){
     quotes = await Quote.findAll({
       attributes: { exclude: ["modifiedAt"] },
@@ -609,12 +608,12 @@ const getUsersAverage = async (req, res) => {
       where: {
           deleted: false,
       },
-    })}
+    });}
     else{
       quotes = await Quote.findAll({
         attributes: { exclude: ["modifiedAt"] },
         include: [
-          { model: QuoteStatus, order: [["id", "DESC"]]},
+          { model: QuoteStatus , order: [["id", "DESC"]]},
           { model: Users, where:{UserRole:{  [sequelize.Op.not]: 'Admin'},deleted: false,} },
         ],
         order: [["id", "DESC"]],
@@ -640,15 +639,10 @@ const getUsersAverage = async (req, res) => {
 
     quotes.map(e=>{
       if(temp[temp.map(object => object.id).indexOf(e.QuoteStatuses[0].UserId)]){
-      e.QuoteStatuses.sort(function(a,b){return b.id-a.id})[0].Status==="Sold" && temp[temp.map(object => object.id).indexOf(e.QuoteStatuses[0].UserId)]?
-
-      temp[temp.map(object => object.id).indexOf(e.QuoteStatuses[0].UserId)].sold= temp[temp.map(object => object.id).indexOf(e.QuoteStatuses[0].UserId)]?.sold+1
-       
-      :(
-      temp[temp.map(object => object.id).indexOf(e.QuoteStatuses[0].UserId)])?
-      temp[temp.map(object => object.id).indexOf(e.QuoteStatuses[0].UserId)].unsold= temp[temp.map(object => object.id).indexOf(e.QuoteStatuses[0].UserId)]?.unsold+1:
-      err++
-      
+      e.QuoteStatuses.sort(function(a,b){return b.id-a.id})[0].Status==="Sold"?
+      temp[temp.map(object => object.id).indexOf(e.QuoteStatuses[0].UserId)].sold= temp[temp.map(object => object.id).indexOf(e.QuoteStatuses[0].UserId)].sold+1
+      :
+      temp[temp.map(object => object.id).indexOf(e.QuoteStatuses[0].UserId)].unsold= temp[temp.map(object => object.id).indexOf(e.QuoteStatuses[0].UserId)].unsold+1
     }})
 
     result = temp.map(e=>{      return{...e, avg: e.sold+e.unsold?
